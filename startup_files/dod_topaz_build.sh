aac2d53dc4892523577daaad313db1db27760444
Jim Galarowicz <jeg@krellinst.org>
2019-08-04 20:19:56 -0500

    Update dod topaz build script.

--- startup_files/dod_topaz_build.sh
+++ startup_files/dod_topaz_build.sh
@@ -1,38 +1,51 @@
-#!/bin/bash 
+#!/bin/bash
 
-export MODULESHOME=/usr/share/modules
-. $MODULESHOME/init/bash
+#BASE_DIR=/p/home/galarowi
+BASE_DIR=/p/home/apps/unsupported/PETtools/CE/pkgs/openss
+TOOL_VERS="_v2.4.1.release.mpich"
+ROOT_VERS="_v2.4.1.release"
+GRAPHVIZ_VERS="-2.40.1"
+QTGRAPH_VERS="-1.0.0"
 
-module unload compiler/intel
-module load compiler/gcc/4.9.3
+OPENMPI_IDIR=/opt/openmpi-2.0.3
+MPT_IDIR=/p/home/apps/hpe/mpt-2.19
+MPICH_IDIR=/p/home/apps/intel/parallel_studio_2019_u2/impi/2019.2.187/intel64
+# Can build multiple mpi, mpit, mpip collectors if desired
+# Must enable these lines with actual paths 
+# and add --with-mpich and/or --with-mvapich2 to the install-tool
+# lines that have --with-openmpi below
+#MVAPICH2_IDIR=<path to mvapich2 install>
+#MPICH_IDIR=<path to mpich install>
 
-export cc=gcc
-export CC=gcc
-export CXX=g++
+KROOT_IDIR=${BASE_DIR}/krellroot${ROOT_VERS}
+CBTF_IDIR=${BASE_DIR}/cbtf${TOOL_VERS}
+OSSCBTF_IDIR=${BASE_DIR}/osscbtf${TOOL_VERS}
+OSSOFF_IDIR=${BASE_DIR}/ossoff${TOOL_VERS}
+#CUDA_IDIR=/usr/local/cuda-6.0
+#CUPTI_IDIR=/usr/local/cuda-6.0/extras/CUPTI
+GRAPHVIZ_IDIR=${BASE_DIR}/graphviz${GRAPHVIZ_VERS}
+QTGRAPH_IDIR=${BASE_DIR}/QtGraph${QTGRAPH_VERS}
+BINUTILS_IDIR=${KROOT_IDIR}/binutils
+MRNET_IDIR=${BASE_DIR}/mrnet_20170810
 
-BASE_DIR=/app/unsupported/PETtools/CE/pkgs/openss
-TOOL_VERS="_v2.3.1.beta1"
-#MPT_DIR=/p/home/apps/sgi/mpt-2.12-sgi712r26
-MPT_DIR=/p/home/apps/sgi/mpt-2.14
-CUDA_IDIR=/p/home/apps/cuda/7.5
-CUPTI_IDIR=/p/home/apps/cuda/7.5/extras/CUPTI
+# Build the externals needed by cbtf and openspeedshop
+#./install-tool --verbose --build-krell-root --krell-root-prefix ${KROOT_IDIR} --with-mpich ${MPICH_IDIR} --with-mpt ${MPT_IDIR} 2>&1 | tee install_tool_build_krell_root.log
 
-##./install-tool --build-mrnet --krell-root-prefix ${BASE_DIR}/krellroot${TOOL_VERS} --with-boost /app/unsupported/PETtools/CE/pkgs/openss/krellroot_v2.3.0cuda
-./install-tool --build-krell-root --krell-root-prefix ${BASE_DIR}/krellroot${TOOL_VERS} --with-mpt ${MPT_DIR} 
+# Build cbtf (base cbtf support) , cbtf-krell (collectors, runtimes), cbtf-argonavis (gpu support), cbtf-lanl (sys admin tools)
+./install-tool --verbose --build-cbtf-all --cbtf-prefix ${CBTF_IDIR} --krell-root-prefix ${KROOT_IDIR} --with-mpich ${MPICH_IDIR} --with-mpt ${MPT_IDIR}  2>&1 | tee install_tool_build_cbtf.log
+#./install-tool --verbose --build-cbtf-all --cbtf-prefix ${CBTF_IDIR} --krell-root-prefix ${KROOT_IDIR} --with-mpich ${MPICH_IDIR} 2>&1 | tee install_tool_build_cbtf.log
 
-##./install-tool --build-offline --openss-prefix ${BASE_DIR}/ossoff${TOOL_VERS} --krell-root-prefix ${BASE_DIR}/krellroot${TOOL_VERS} --with-mpt ${MPT_DIR}
+# Build openspeedshop using cbtf components and the root external packages
+./install-tool --verbose --build-oss --openss-prefix ${OSSCBTF_IDIR} --cbtf-prefix ${CBTF_IDIR} --krell-root-prefix ${KROOT_IDIR} --with-mpich ${MPICH_IDIR} --with-mpt ${MPT_IDIR} 2>&1 | tee install_tool_build_oss.log
+#./install-tool --verbose --build-oss --openss-prefix ${OSSCBTF_IDIR} --cbtf-prefix ${CBTF_IDIR} --krell-root-prefix ${KROOT_IDIR} --with-mpich ${MPICH_IDIR} 2>&1 | tee install_tool_build_oss.log
 
-./install-tool --build-cbtf-all --cbtf-prefix ${BASE_DIR}/cbtf${TOOL_VERS} --krell-root-prefix ${BASE_DIR}/krellroot${TOOL_VERS} --with-mpt /${MPT_DIR} --with-cuda ${CUDA_IDIR} --with-cupti ${CUPTI_IDIR} --with-boost ${BASE_DIR}/krellroot${TOOL_VERS}
+# IF BUILDING THE NEW GUI INTERFACE
 
-./install-tool --build-oss --openss-prefix ${BASE_DIR}/osscbtf${TOOL_VERS} --cbtf-prefix ${BASE_DIR}/cbtf${TOOL_VERS} --krell-root-prefix ${BASE_DIR}/krellroot${TOOL_VERS} --with-mpt ${MPT_DIR} --with-cuda ${CUDA_IDIR} --with-cupti ${CUPTI_IDIR} --with-boost ${BASE_DIR}/krellroot${TOOL_VERS}
+# Build graphviz for support of building the new cbtf-argonavis-gui
+#./install-tool --build-graphviz --krell-root-prefix ${GRAPHVIZ_IDIR}
 
+# Build QtGraph for support of building the new cbtf-argonavis-gui, uses graphviz
+#./install-tool --build-QtGraph --krell-root-prefix ${QTGRAPH_IDIR} --with-graphviz ${GRAPHVIZ_IDIR} --with-qt /usr/lib64/qt5
 
-# Need this when executing OSS on compute nodes.   Non-standard installation compiler usage requires the module be loaded at run time or having the libstdc++ library available 
-cp /apps/gnu_compiler/4.9.3/lib64/libstdc++.so.6 ${BASE_DIR}/krellroot${TOOL_VERS}/lib64/libstdc++.so.6
-
-#export KRELL_ROOT_BOOST=/app/unsupported/PETtools/CE/pkgs/openss/krellroot_v2.3.0
-#export BOOST_ROOT=/app/unsupported/PETtools/CE/pkgs/openss/krellroot_v2.3.0
-#export QTDIR=/usr/lib64/qt4
-#./install-tool --build-cbtfargonavisgui --with-openss ${BASE_DIR}/osscbtf${TOOL_VERS} --with-cbtf ${BASE_DIR}/cbtf${TOOL_VERS} --krell-root-prefix ${BASE_DIR}/krellroot${TOOL_VERS} --with-boost ${BASE_DIR}/krellroot${TOOL_VERS} 
-
-
+# Build new cbtf-argonavis-gui, uses graphviz and QtGraph 
+#./install-tool --build-cbtfargonavisgui --with-openss ${OSSCBTF_IDIR} --with-cbtf ${CBTF_IDIR} --krell-root-prefix ${KROOT_IDIR} --with-graphviz ${GRAPHVIZ_IDIR} --with-QtGraph ${QTGRAPH_IDIR} --with-boost ${KROOT_IDIR} --with-qt /usr/lib64/qt5 2>&1 | tee install_tool_build_cbtf_gui.log
